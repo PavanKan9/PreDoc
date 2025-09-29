0331535086523d19299c4587962591e4f20ca3ab
PavanKan9 <pavankanumalla@icloud.com>
2025-09-29 14:34:37 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -28,7 +28,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=ALLOW_ORIGINS,
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
